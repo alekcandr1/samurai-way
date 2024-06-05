4725c67a4842ec3c8fcc9a8533d31d67d7203932
alekcandr1 <alekcandrmain@gmail.com>
2024-06-05 14:53:28 +0300

    refactoring

--- src/components/mainContent/MainContent.tsx
+++ src/components/mainContent/MainContent.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Profile from './profile/Profile';
-import Dialogs from './dialogs/Dialogs';
 import News from './news/News';
 import Music from './music/Music';
 import Settings from './settings/Settings';
 import classes from './profile/Profile.module.css';
 import { Route } from 'react-router-dom';
+import DialogsContainer from './dialogs/DialogsContainer';
 
 type MainContentProps = {}
 
@@ -13,7 +13,7 @@ const MainContent = ( props: MainContentProps ) => {
     return (
         <main className={ classes.content }>
             <Route exact path={ '/profile' } render={ () => <Profile /> } />
-            <Route path={ '/dialogs' } render={ () => <Dialogs /> } />
+            <Route path={ '/dialogs' } render={ () => <DialogsContainer /> } />
             <Route exact path={ '/news' } render={ () => <News /> } />
             <Route exact path={ '/music' } render={ () => <Music /> } />
             <Route exact path={ '/settings' } render={ () => <Settings /> } />
